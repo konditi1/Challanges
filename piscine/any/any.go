f98072345ec3fb6a69dd0ed6c5d4d8b2c3a893ba
fonditi <konditi1.com>
2024-05-04 16:55:54 +0300

    function Any takes a function literal and slice the returns true if any condtion is true

---
+++ piscine/any/any.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func Any(f func(string) bool, a []string) bool {
+	for _, v := range a {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func IsNumeric(str string) bool {
+	if len(str) == 0 {
+		return false
+	}
+	for _, c := range str {
+		if c < '0' || c > '9' {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	a1 := []string{"Hello", "how", "44", "you"}
+	a2 := []string{"This", "i4s", "on", "you"}
+
+	result1 := Any(IsNumeric, a1)
+	result2 := Any(IsNumeric, a2)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
+}
